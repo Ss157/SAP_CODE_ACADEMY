7095954c57de074689098b49d9fda61c5e04aceb
Ss157 <107520248+Ss157@users.noreply.github.com>
2026-01-13 17:34:47 +0900

    Delete Fiori/webapp directory

--- Fiori/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-QUnit.config.autostart = false;
-
-sap.ui.require(["code1/cl5/calculator/code1cl5calculator/test/integration/AllJourneys"
-], function () {
-	QUnit.start();
-});
--- Fiori/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"code1/cl5/calculator/code1cl5calculator/test/unit/controller/CalView.controller"
-], function () {
-	"use strict";
-});
